1f136530ae81290f036b64a1b6df1591326e9ee0
Arwid Thornstrom <arwidt@gmail.com>
2016-11-03 07:41:39 +0100

    some changes

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,18 @@ var del = require('./index.js');
 var copy = require('taskmodule__copy');
 
 gulp.task('default', function() {
-    
+
     async.series([
         copy('test/file.txt', 'target/newfile.txt'),
         copy('test/folder', 'target/newfolder'),
+        function(callback) {
+            setTimeout(function() {
+                callback();
+            }, 2000);
+        },
         del(['target'])
     ], function() {
         console.log("DONE");
-    });    
+    });
 
 });
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var del = require('del');
 
 module.exports = function(paths) {
     return function(callback) {
+        var del = require('del');
+
         del(paths).then(function(resp) {
             callback();
         });
